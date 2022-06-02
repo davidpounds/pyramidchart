61537bbe01be2b6b7a8da8b32ad3f3f465da813d
David Pounds <david@davidpounds.com>
2022-06-02 23:35:33 +0100

    Converts to class and updates demo and README

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# pyramidchart
-Javascript driven SVG Pyramid chart for McNeil Collective
+# pyramid-chart
+A JavaScript-driven SVG pyramid chart.
 
-See demo folder for a example usage.
+First include the `src/pyramid-chart.js` file before the chart initialisation code. This gives you access to the `PyramidChart` class.
 
+The `PyramidChart` class is constructed like this: `new PyramidChart(data, width, height)` where:
+
+* `data [object[], required]` is an array of properties for each slice containing the following properties:
+    * `color [string, required]` - This sets the colour of the slice. Any valid SVG colour can be used (e.g. hex, rgb, named colours etc)
+    * `label [string, required]` - This is the text that will be displayed on hover for the slice.
+    * `rate [string, optional]` - If provided, this will be displayed after the label on hover for the slice.
+    * `percent [number, required]` - This is displayed by default on the slice, and also appears after the label and rate on hover for the slice.
+    * `height [number, optional]` - If provided, this is a fractional heigt for the slice. The height of each slice is the percentage of the current slice's height compared to the sum of the height of all slices. If you provide this value for at least one slice you should provide it for all slices.
+* `width [number, optional]` - The width of the chart in pixels. If ommited, the default value of 400 will be used.
+* `height [number, optional]` - The height of the chart in pixels. If ommited, the default value of 400 will be used.
+
+The `PyramidChart` instance has a method named `getPyramidChart()` that will return an SVG element for the chart.
+
+E.g. `const svg = (new PyramidChart(data, width, height)).getPyramidChart();` 
+
+The `svg` variable will contain an SVG element that can be added to a container element using the element's `appendChild()` method:
+
+`document.querySelector('#chart-container').appendChild(svg);` where the chart's containing element has an ID of `chart-container`.
--- src/pyramid-chart.js
+++ src/pyramid-chart.js
@@ -1,109 +1,114 @@
-const SVG_NAMESPACE = "http://www.w3.org/2000/svg";
+class PyramidChart {
 
-const setAttributes = (element, attributes) => {
-    // Warning - this function is not pure, it mutates the element
-    Object.entries(attributes).forEach(([key, value]) => {
-        element.setAttribute(key, value);
-    });
-};
-
-const setSelectedIndex = (svg, selectedIndex) => {
-    svg.dataset.selectedIndex = selectedIndex;
-};
-
-const createTextNode = (x, y, textValue, dataAttribute, index) => {
-    const text = document.createElementNS(SVG_NAMESPACE, "text");
-    setAttributes(text, {
-        x,
-        y,
-        [dataAttribute]: index,
-    });
-    text.appendChild(document.createTextNode(textValue));
-    return text;
-};
-
-const generateStyles = (data) => {
-    const labelSelectors = [];
-    const tooltipSelectors = [];
-    for (let i = 0; i < data.length; i++) {
-        labelSelectors.push(`[data-selected-index="${i}"] [data-label="${i}"]`);
-        tooltipSelectors.push(`[data-selected-index="${i}"] [data-tooltip="${i}"]`);
+    constructor(data, width = 400, height = 400) {
+        this.width = width;
+        this.height = height;
+        this.data = data;
+        this.SVG_NAMESPACE = "http://www.w3.org/2000/svg";
+        this.svg = null;
     }
-    return `${labelSelectors.join(', ')} {
-        display: none;
-    } 
-    ${tooltipSelectors.join(', ')} {
-        display: initial;
-    }`;
-};
 
-const generatePyramidChart = (data, width = 400, height = 400) => {
-    const svg = document.createElementNS(SVG_NAMESPACE, "svg");
-    setSelectedIndex(svg, -1);
-    setAttributes(svg, {
-        xmlns: SVG_NAMESPACE,
-        viewBox: `0 0 ${width} ${height}`,
-        width: width,
-        height: height,
-    });
-    const defs = document.createElementNS(SVG_NAMESPACE, "defs");
-    const clipPathId = "triangle";
-    const clipPath = document.createElementNS(SVG_NAMESPACE, "clipPath");
-    clipPath.setAttribute("id", clipPathId);
-    const path = document.createElementNS(SVG_NAMESPACE, "path");
-    path.setAttribute("d", `M0,${height} L${width / 2},0 L${width},${height}Z`);
-    defs.appendChild(clipPath);
-    clipPath.appendChild(path);
-
-    const style = document.createElementNS(SVG_NAMESPACE, "style");
-    svg.appendChild(style);
-    style.innerHTML =`
-        text {
-            dominant-baseline: middle;
-            text-anchor: middle;
-            fill: #000;
-        }
-        [data-tooltip] { 
-            display: none; 
-        }
-        ${generateStyles(data)}`;
-
-    const g = document.createElementNS(SVG_NAMESPACE, "g");
-    g.setAttribute("clip-path", `url(#${clipPathId})`);
-    svg.appendChild(g);
-    
-    const rectHeightSum = data.map(d => d.height).reduce((sum, h) => sum + (h ?? 0), 0);
-    let accumulatedHeight = 0;
-    data.forEach((d, i) => {
-        const heightPercent = rectHeightSum > 0 ? d.height / rectHeightSum : 1 / data.length;
-        const rectHeight = height * heightPercent;
-        const startY = accumulatedHeight;
-        const rectangle = document.createElementNS(SVG_NAMESPACE, "rect");
-        setAttributes(rectangle, {
-            x: 0,
-            y: startY,
-            width: width,
-            height: rectHeight,
-            stroke: 'none',
-            fill: d.color,
+    setAttributes(element, attributes) {
+        // Warning - this function is not pure, it mutates the element
+        attributes.forEach((value, key) => {
+            element.setAttribute(key, value);
         });
-        rectangle.onmouseover = function () {
-            setSelectedIndex(svg, i);
-        };
-        rectangle.onmouseleave = function () {
-            setSelectedIndex(svg, -1);
-        };
-        accumulatedHeight += rectHeight;
-        const text = createTextNode(width / 2, startY + rectHeight / 2, `${d.percent}%`, 'data-label', i);
-        const rate = d.rate ? `(Rate: ${d.rate})` : ''
-        const hoverText = createTextNode(width / 2, startY + rectHeight / 2, `${d.type} ${rate}: ${d.percent}%`, 'data-tooltip', i);
-        hoverText.onmouseenter = function() {
-            setSelectedIndex(svg, i);
-        };
-        g.appendChild(rectangle);
-        svg.appendChild(text);
-        svg.appendChild(hoverText);
-    });
-    svg.appendChild(defs);
-    return svg;
-};
+    }
+
+    setSelectedIndex (svg, selectedIndex) {
+        svg.dataset.selectedIndex = selectedIndex.toString();
+    }
+
+    createTextNode (x, y, textValue, dataAttribute, index) {
+        const text = document.createElementNS(this.SVG_NAMESPACE, "text");
+        this.setAttributes(text, new Map([
+            ['x', x],
+            ['y', y],
+            [dataAttribute, index],
+        ]));
+        text.appendChild(document.createTextNode(textValue));
+        return text;
+    }
+
+    generateStyles (data) {
+        const labelSelectors = [];
+        const tooltipSelectors = [];
+        for (let i = 0; i < data.length; i++) {
+            labelSelectors.push(`[data-selected-index="${i}"] [data-label="${i}"]`);
+            tooltipSelectors.push(`[data-selected-index="${i}"] [data-tooltip="${i}"]`);
+        }
+        return `${labelSelectors.join(', ')} {
+            display: none;
+        } 
+        ${tooltipSelectors.join(', ')} {
+            display: initial;
+        }`;
+    }
+
+    getPyramidChart() {
+        this.svg = document.createElementNS(this.SVG_NAMESPACE, "svg");
+        this.setSelectedIndex(this.svg, -1);
+        this.setAttributes(this.svg, new Map([
+            ['xmlns', this.SVG_NAMESPACE],
+            ['viewBox', `0 0 ${this.width} ${this.height}`],
+            ['width', this.width],
+            ['height', this.height],
+        ]));
+
+        const style = document.createElementNS(this.SVG_NAMESPACE, "style");
+        this.svg.appendChild(style);
+        style.innerHTML =`
+            g {
+                clip-path: polygon(0% 100%, 50% 0%, 100% 100%, 0% 100%);
+            }
+            text {
+                dominant-baseline: middle;
+                text-anchor: middle;
+                fill: #000;
+            }
+            [data-tooltip] { 
+                display: none; 
+            }
+            ${this.generateStyles(this.data)}`;
+
+        const g = document.createElementNS(this.SVG_NAMESPACE, "g");
+        this.svg.appendChild(g);
+        
+        const rectHeightSum = this.data.map(d => d.height).reduce((sum, h) => sum + (h ?? 0), 0);
+        let accumulatedHeight = 0;
+        this.data.forEach((d, i) => {
+            const heightPercent = rectHeightSum > 0 ? d.height / rectHeightSum : 1 / this.data.length;
+            const rectHeight = this.height * heightPercent;
+            const startY = accumulatedHeight;
+            const rectangle = document.createElementNS(this.SVG_NAMESPACE, "rect");
+            this.setAttributes(rectangle, new Map([
+                ['x', '0'],
+                ['y', startY],
+                ['width', this.width],
+                ['height', rectHeight],
+                ['stroke', 'none'],
+                ['fill', d.color],
+            ]));
+            rectangle.onmouseover = () => {
+                this.setSelectedIndex(this.svg, i);
+            };
+            rectangle.onmouseleave = () => {
+                this.setSelectedIndex(this.svg, -1);
+            };
+            accumulatedHeight += rectHeight;
+            const text = this.createTextNode(this.width / 2, startY + rectHeight / 2, `${d.percent}%`, 'data-label', i);
+            const rate = d.rate ? `(Rate: ${d.rate})` : ''
+            const hoverText = this.createTextNode(this.width / 2, startY + rectHeight / 2, `${d.label} ${rate}: ${d.percent}%`, 'data-tooltip', i);
+            hoverText.onmouseenter = () => {
+                this.setSelectedIndex(this.svg, i);
+            };
+            hoverText.onmouseleave = () => {
+                this.setSelectedIndex(this.svg, -1);
+            };
+            g.appendChild(rectangle);
+            this.svg.appendChild(text);
+            this.svg.appendChild(hoverText);
+        });
+        return this.svg;
+    }
+}
